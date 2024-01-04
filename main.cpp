a3f0db9fa6499429715ecd866b8b7e4398c3e8e2
Oleksii Zibarov <pasazhir23@gmail.com>
2024-01-04 22:18:25 +0200

    add error checks

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 
+
 int main()
 {
     int op = 0;
@@ -8,15 +9,29 @@ int main()
     std::cout << "viberit operaciu(1 - plus; 2 - minus): ";
     std::cin >> op;
 
+    if((op!=1 && op!=2) || std::cin.fail()){
+        std::cout << "\nerror\n";
+        return 1;
+    }
+
     double a = 0;
     double b = 0;
 
     std::cout << "vvedit chislo a: ";
     std::cin >> a;
 
+    if (std::cin.fail()){
+        std::cout << "\nerror\n";
+        return 1;
+    }
+
     std::cout << "vvedit chislo b: ";
     std::cin >> b;
 
+    if (std::cin.fail()){
+        std::cout << "\nerror\n";
+        return 1;
+    }
     switch (op) {
 
     case 1:
